f3c82cf0a146b2cbc99e5ab2f7291bb45952b6c4
Sagar Rane <sagar.rane@liveramp.com>
2024-06-11 20:57:07 -0700

    Ensure promise resolves even if label exists

--- src/applyLabels.ts
+++ src/applyLabels.ts
@@ -3,34 +3,62 @@ import * as github from '@actions/github'
 import * as octokit from '@octokit/rest'
 import {Label} from './getLabelsFromOwners'
 
-export async function applyLabels(
-  context: Context,
-  client: github.GitHub,
-  labels: Set<Label>
-): Promise<void> {
-  // create labels if they don't exist
-  const p: Promise<octokit.Response<octokit.IssuesCreateLabelResponse>>[] = []
-  // store labels in a list; will be used later
-  const labelsAll: string[] = []
-  try {
+// export async function applyLabels(
+//   context: Context,
+//   client: github.GitHub,
+//   labels: Set<Label>
+// ): Promise<void> {
+//   // create labels if they don't exist
+//   const p: Promise<octokit.Response<octokit.IssuesCreateLabelResponse>>[] = []
+//   // store labels in a list; will be used later
+//   const labelsAll: string[] = []
+//   try {
+//     for (const label of labels) {
+//       labelsAll.push(label.name)
+//       p.push(
+//         client.issues.createLabel({
+//           owner: context.issue.owner,
+//           repo: context.issue.repo,
+//           name: label.name,
+//           color: label.color
+//         })
+//       )
+//     }
+//     await Promise.all(p)
+//   } catch (error) {
+//     // if 422, label already exists
+//     if (error.status !== 422) {
+//       throw error
+//     }
+//   }
+
+
+  export async function applyLabels(
+    context: Context,
+    client: github.GitHub,
+    labels: Set<Label>
+  ): Promise<void> {
+    const p: Promise<octokit.Response<octokit.IssuesCreateLabelResponse>>[] = []
+    const labelsAll: string[] = []
+  
     for (const label of labels) {
       labelsAll.push(label.name)
       p.push(
         client.issues.createLabel({
-          owner: context.issue.owner,
-          repo: context.issue.repo,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
           name: label.name,
           color: label.color
+        }).catch(error => {
+          if (error.status !== 422) { // if 422, label already exists
+            throw error
+          }
         })
       )
     }
+  
+    // Wait for all label creation requests to complete
     await Promise.all(p)
-  } catch (error) {
-    // if 422, label already exists
-    if (error.status !== 422) {
-      throw error
-    }
-  }
 
   // apply labels to the PR
   // don't even try if no labels
